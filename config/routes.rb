e33fb379a8e8ee05a0a09b1424fea23a46bda568
Vladislav <lvo123ua@gmail.com>
2018-10-13 22:31:45 +0300

    Hard Reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,2 @@
 Rails.application.routes.draw do
-  resource :profile, only: [:create, :destroy, :show, :update]
-  resource :session, only: [:create, :destroy]
-  resources :users,  only: [:show, :index]
-  resources :questions, only: [:show, :create, :update, :index] do
-    resource :rate, only: :create, modules: :questions
-    resource :answers, only: [:create, :show]
-  end
-
-  post 'answers/:answers_id/rate', to: 'rates#create', modules: :answers
 end
